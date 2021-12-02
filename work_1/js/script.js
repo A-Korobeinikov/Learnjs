76bf7281fe9aac6d16523656cae3e5e12a41c4b3
Anton Korobeinikov <an.korobeinikov@mail.ru>
2021-12-02 14:35:01 +0300

    Condition while

--- work_1/js/script.js
+++ work_1/js/script.js
@@ -10,12 +10,52 @@ let personalMovieDB = {
     privat: false,
 };
 
-let a = prompt('One of the last watching films', '');
-let b = prompt('На сколько оцените его?', '');
-let c = prompt('One of the last watching films', '');
-let d = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('One of the last watching films', '');
+//     const b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+ 
+// }
 
-console.log(personalMovieDB);
+
+
+ // Реализация через While
+let i = 0 ;
+while (i < 2) {
+    
+    const a = prompt('One of the last watching films', '');
+    const b = prompt('На сколько оцените его?', '');    
+    
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    personalMovieDB.movies[a] = b;
+    i++;
+    
+}
+
+
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель'); 
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман!');
+// } else {
+//     console.log('Что-то пошло не так!');
+// }
+
+ console.log(personalMovieDB);
